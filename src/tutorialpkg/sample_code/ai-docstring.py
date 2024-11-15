f993a8c7d3c2ba1c383ea9c725fa5a48d9671b4c
Your Name <you@example.com>
2024-11-15 16:00:31 +0000

    mermaid

--- src/tutorialpkg/sample_code/ai-docstring.py
+++ src/tutorialpkg/sample_code/ai-docstring.py
@@ -1,5 +1,5 @@
 
-    """ Prints information that the describes the contents of the DataFrame.
+""" Prints information that the describes the contents of the DataFrame.
 
       - Print the number of rows and columns in the DataFrame using `.shape`
       - Print the first 7 rows of data using `.head()` and the last 6 rows using `.tail()`
@@ -9,7 +9,4 @@
 
     Args:
         df: A pandas dataframe containing the data
-    """
-
-
-
+        """
--- src/tutorialpkg/sample_code/get_cols_dtypes.py
+++ src/tutorialpkg/sample_code/get_cols_dtypes.py
@@ -7,18 +7,20 @@ from pathlib import Path
 import pandas as pd
 
 if __name__ == '__main__':
-    # csv
-    try:
-        paralympics_datafile_csv = Path(__file__).parent.parent.joinpath("data", "paralympics_events_prepared.csv")
-        df_paralympics = pd.read_csv(paralympics_datafile_csv)
-        print(df_paralympics.dtypes)
-    except FileNotFoundError as e:
-        print(f"paralympics_events_prepared.csv file not found. Please check the file path. Error: {e}")
-
-    # excel
+     # excel
     try:
         paralympics_datafile_excel = Path(__file__).parent.parent.joinpath("data", "paralympics_events_prepared.xlsx")
         df_paralympics_e = pd.read_excel(paralympics_datafile_excel)
         print(df_paralympics_e.dtypes)
     except FileNotFoundError as e:
         print(f"paralympics_events_prepared.xlsx file not found. Please check the file path. Error: {e}")
+
+    # csv
+'''    try:
+        paralympics_datafile_csv = Path(__file__).parent.parent.joinpath("data", "paralympics_events_prepared.csv")
+        df_paralympics = pd.read_csv(paralympics_datafile_csv)
+        print(df_paralympics.dtypes)
+    except FileNotFoundError as e:
+        print(f"paralympics_events_prepared.csv file not found. Please check the file path. Error: {e}")'''
+
+   
